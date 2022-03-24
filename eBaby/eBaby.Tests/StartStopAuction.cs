116d64f0e2fc61b363715da07bc5e5cce6137196
C# Mob <fake@example.com>
2022-03-24 16:51:57 +0000

    Add Start Stop Auction

---
+++ eBaby/eBaby.Tests/StartStopAuction.cs
@@ -0,0 +1,33 @@
+using System;
+using ApprovalTests;
+using FluentAssertions;
+using Xunit;
+
+namespace eBaby.Tests
+{
+    public class StartStopAuction
+    {
+        [Fact]
+        public void UserCanCreateAuction()
+        {
+            var user = Arbitrary.UserWithUserName("right_user");
+            user.BecomeSeller();
+            var clock = new StoppedClock();
+            var result = user.CreateAuction("ItemDescr", 23.95m, clock.Now()
+                , clock.Now());
+            Approvals.Verify(result);
+        }
+
+        [Fact]
+        public void User_must_be_a_seller_to_create_an_auction()
+        {
+            var user = Arbitrary.RegisteredUser(out var registry);
+            Action makeSeller = () =>
+            {
+                var clock = new StoppedClock();
+                user.CreateAuction("ItemDescr", 23.95m, clock.Now(), clock.Now());
+            };
+            makeSeller.Should().Throw<NotAuthorizedException>();
+        }
+    }
+}
--- eBaby/eBaby.Tests/UserTest.cs
+++ eBaby/eBaby.Tests/UserTest.cs
@@ -7,6 +7,7 @@ namespace eBaby.Tests
 {
     public class UserTest
     {
+
         [Fact]
         public void User_Creation()
         {
@@ -48,29 +49,6 @@ namespace eBaby.Tests
             foundUser.Should().BeSameAs(otherUser);
         }
 
-        [Fact]
-        public void UserCanCreateAuction()
-        {
-            var user = Arbitrary.UserWithUserName("right_user");
-            user.BecomeSeller();
-            var clock = new StoppedClock();
-            var result = user.CreateAuction("ItemDescr", 23.95m, clock.Now()
-                , clock.Now());
-            Approvals.Verify(result);
-        }
-
-        [Fact]
-        public void User_must_be_a_seller_to_create_an_auction()
-        {
-            var user = Arbitrary.RegisteredUser(out var registry);
-            Action makeSeller = () =>
-            {
-                var clock = new StoppedClock();
-                user.CreateAuction("ItemDescr", 23.95m, clock.Now(), clock.Now());
-            };
-            makeSeller.Should().Throw<NotAuthorizedException>();
-        }
-        
         [Fact]
         public void User_Login_Success()
         {
