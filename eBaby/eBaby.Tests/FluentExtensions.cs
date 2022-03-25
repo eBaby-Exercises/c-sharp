175d5840e2c029a3f83777ec8d25688f8bf4fbc0
C# Mob <fake@example.com>
2022-03-25 15:35:09 +0000

    Added Expressive Assertion Msg for PostOffice

--- eBaby/eBaby.Tests/FluentExtensions.cs
+++ eBaby/eBaby.Tests/FluentExtensions.cs
@@ -1,3 +1,7 @@
+using eBabyServices;
+using FluentAssertions;
+using FluentAssertions.Primitives;
+
 namespace eBaby.Tests
 {
     static internal class FluentExtensions
@@ -5,6 +9,27 @@ namespace eBaby.Tests
         public static AuctionAssertions Should(this Auction testSubject)
         {
             return new AuctionAssertions(testSubject);
+        }
+
+        public static PostOfficeAssertions Should(this PostOffice postOffice)
+        {
+            return new PostOfficeAssertions(postOffice);
+        }
+    }
+
+    internal class PostOfficeAssertions : ReferenceTypeAssertions<PostOffice, PostOfficeAssertions>
+    {
+        public PostOfficeAssertions(PostOffice subject) : base(subject)
+        {
+
+        }
+
+        protected override string Identifier => "PostOffice";
+
+        public void HaveSeenMsg(Auction testSubject)
+        {
+            Subject.FindEmail(testSubject.Seller.UserEmail, string.Empty).Should()
+                .Be($"<sendEMail address=\"{testSubject.Seller.UserEmail}\" >Hi</sendEmail>\n");
         }
     }
 }
--- eBaby/eBaby.Tests/StartStopAuction.cs
+++ eBaby/eBaby.Tests/StartStopAuction.cs
@@ -43,7 +43,7 @@ namespace eBaby.Tests
             testSubject.OnStart();
             testSubject.OnClose();
             var postOffice = PostOffice.GetInstance();
-            postOffice.FindEmail(testSubject.Seller.UserEmail, string.Empty).Should().Be($"<sendEMail address=\"{testSubject.Seller.UserEmail}\" >Hi</sendEmail>\n");
+            postOffice.Should().HaveSeenMsg(testSubject);
         }
 
         [Fact]
